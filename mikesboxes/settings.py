937cd02806e2f6edb89da968a23bed4e05ac532f
Simon Quigley <squigley@altispeed.com>
2024-01-23 10:16:54 -0600

    Also add to the templates to be searched for

--- mikesboxes/settings.py
+++ mikesboxes/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = "mikesboxes.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "boxes/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
