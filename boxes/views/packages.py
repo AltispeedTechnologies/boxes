74b8e48d7f4a1b996c44905a58aa8dd01b45a779
Simon Quigley <squigley@altispeed.com>
2024-02-13 14:10:52 -0600

    Many table-related updates, for hyperefficient queries

--- boxes/views/packages.py
+++ boxes/views/packages.py
@@ -1,45 +1,28 @@
+from django.shortcuts import render
 from boxes.forms import PackageForm
 from django.contrib import messages
 from django.core.validators import validate_slug
 from django.core.exceptions import ValidationError
-from django.http import HttpResponseBadRequest
-from django.shortcuts import redirect, render
-from boxes.models import Address, Package, PACKAGE_STATES
+from django.shortcuts import redirect
+from boxes.models import Package, PACKAGE_STATES
 
 # Reverse the PACKAGE_STATES tuple to create a mapping from state names to IDs
 STATE_NAME_TO_ID_MAP = dict((name, id) for id, name in PACKAGE_STATES)
+CONTEXT = {"STATE_NAMES": STATE_NAME_TO_ID_MAP}
 
 def all_packages(request):
-    packages = Package.objects.select_related("account", "address").values(
-        "id",
-        "account__description",
-        "address__address",
-        "tracking_code",
-        "current_state",
-    ).all()
-
-    # This is usually done automatically, but manually specified due to the
-    # above query being precise
-    for package in packages:
-        package["current_state_display"] = dict(PACKAGE_STATES)[package["current_state"]]
-
-    return render(request, "packages/index.html", {"packages": packages})
+    packages = _get_packages()
+    return render(request, "packages/index.html", {"packages": packages, **CONTEXT})
 
 def create_package(request):
-    if request.method == "POST":
-        form = PackageForm(request.user, request.POST)
-        if form.is_valid():
-            # Process the form data and save the new package
-            package = form.save(commit=False)
-            package.user = request.user.id
-            package.account_id = 1
-            package.address_id = 1
-            package.save()
-
-            return redirect("packages")
-    else:
-        form = PackageForm(request.user)
-
+    form = PackageForm(request.user, data=request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        package = form.save(commit=False)
+        package.user = request.user.id
+        package.account_id = 1
+        package.address_id = 1
+        package.save()
+        return redirect("packages")
     return render(request, "packages/create.html", {"form": form})
 
 def search_packages(request):
@@ -58,41 +41,43 @@ def search_packages(request):
         return redirect("packages")
 
     if filters == "tracking_code":
-        # Find all packages containing this tracking code
         packages = search_by_tracking_code(query)
     elif filters == "current_state":
-        # Find all packages with matching states
-        packages = search_by_current_state(query)
+        packages = search_by_current_state(request, query)
 
-    return render(request, "packages/search.html", {"packages": packages, "query": query, "filter": filters})
+    return render(request, "packages/search.html", {"packages": packages, "query": query, "filter": filters, **CONTEXT})
 
 def search_by_tracking_code(query):
-    packages = Package.objects.filter(tracking_code__icontains=query).values(
-        "id",
-        "account__description",
-        "address__address",
-        "tracking_code",
-        "current_state",
-    )
-    add_current_state_display(packages)
+    packages = _get_packages(tracking_code__icontains=query)
     return packages
 
-def search_by_current_state(query):
-    matching_states = [state_id for name, state_id in STATE_NAME_TO_ID_MAP.items() if query.lower() in name.lower()]
+def search_by_current_state(request, query):
+    # Remove leading and trailing whitespace from the query string value
+    selected_state_name = query.strip()
+    
+    # Find matching states with flexible whitespace handling
+    matching_states = [state_name.strip().lower() for state_name in STATE_NAME_TO_ID_MAP.keys() if selected_state_name.lower() in state_name.lower()]
+    
     if matching_states:
-        packages = Package.objects.filter(current_state__in=matching_states).values(
-            "id",
-            "account__description",
-            "address__address",
-            "tracking_code",
-            "current_state",
-        )
-        add_current_state_display(packages)
+        # Get the corresponding IDs for the matching state names
+        matching_state_ids = [STATE_NAME_TO_ID_MAP[state_name] for state_name in matching_states]
+        packages = _get_packages(current_state__in=matching_state_ids)
         return packages
     else:
         messages.error(request, "Invalid state name")
         return redirect("packages")
 
-def add_current_state_display(packages):
+def _get_packages(**kwargs):
+    packages = Package.objects.select_related("account", "address").values(
+        "id",
+        "account__description",
+        "address__address",
+        "tracking_code",
+        "current_state",
+    ).filter(**kwargs)
+    _add_current_state_display(packages)
+    return packages
+
+def _add_current_state_display(packages):
     for package in packages:
         package["current_state_display"] = dict(PACKAGE_STATES)[package["current_state"]]
