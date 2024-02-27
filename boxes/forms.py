b01bde85f5b3aea679b3f60f3a99abb0a3451b54
Simon Quigley <squigley@altispeed.com>
2024-02-27 13:09:59 -0600

    Clean up form code and display of prices, ensure that validation is Right

--- boxes/forms.py
+++ boxes/forms.py
@@ -1,15 +1,36 @@
 from boxes.models import CustomUser, Package, Carrier, Account, PackageType
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from django.core.validators import MaxLengthValidator
 
+class PriceInput(forms.widgets.TextInput):
+    def __init__(self, attrs=None):
+        if attrs is None:
+            attrs = {}
+        attrs.update({
+            "type": "number",
+            "step": "0.01",
+            "value": "6.00",
+            "placeholder": "6.00",
+            "class": "form-control"
+        })
+        super().__init__(attrs)
+
+
 class PriceField(forms.DecimalField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", PriceInput())
+        super().__init__(*args, **kwargs)
+
     def to_python(self, value):
         value = super().to_python(value)
         if value is not None:
+            if value < 0:
+                raise ValidationError("Price cannot be negative.")
             max_digits, decimal_places = 8, 2
-            value_str = '{:.{}f}'.format(value, decimal_places)
-            int_part, dec_part = value_str.split('.')
+            value_str = "{:.{}f}".format(value, decimal_places)
+            int_part, dec_part = value_str.split(".")
             if len(dec_part) > decimal_places:
                 raise ValidationError("No more than {} decimal places are allowed.".format(decimal_places))
             if len(int_part) > max_digits - decimal_places:
@@ -25,13 +46,20 @@ class RegisterForm(UserCreationForm):
 
 class PackageForm(forms.ModelForm):
     tracking_code = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
-    price = PriceField(widget=forms.NumberInput(attrs={"class": "form-control"}))
+    price = PriceField()
     comments = forms.CharField(widget=forms.Textarea(attrs={"rows": 2, "cols": 2}),
-                               validators=[MaxLengthValidator(256)])
+                               validators=[MaxLengthValidator(256)],
+                               required=False)
 
-    carrier_id = forms.ChoiceField(label="Carrier", choices=Carrier.objects.values_list("id", "name").distinct(), widget=forms.Select(attrs={"class": "form-control select-select2"}))
-    account_id = forms.ChoiceField(label="Customer", choices=Account.objects.values_list("id", "description").distinct(), widget=forms.Select(attrs={"class": "form-control select-select2"}))
-    package_type_id = forms.ChoiceField(label="Type", choices=PackageType.objects.values_list("id", "description").distinct(), widget=forms.Select(attrs={"class": "form-control select-select2"}))
+    carrier_id = forms.ChoiceField(label="Carrier",
+                                   choices=Carrier.objects.values_list("id", "name").distinct(),
+                                   widget=forms.Select(attrs={"class": "form-control select-select2"}))
+    account_id = forms.ChoiceField(label="Customer",
+                                   choices=Account.objects.values_list("id", "description").distinct(),
+                                   widget=forms.Select(attrs={"class": "form-control select-select2"}))
+    package_type_id = forms.ChoiceField(label="Type",
+                                        choices=PackageType.objects.values_list("id", "description").distinct(),
+                                        widget=forms.Select(attrs={"class": "form-control select-select2"}))
 
     class Meta:
         model = Package
