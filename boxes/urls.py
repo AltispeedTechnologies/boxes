227f5431f8ec916ad15205446f2f1c79132d213b
Simon Quigley <squigley@altispeed.com>
2024-04-03 05:48:46 +0000

    more comments is better

--- boxes/urls.py
+++ boxes/urls.py
@@ -8,19 +8,30 @@ def is_staff(view_func):
 
 urlpatterns = [
     path("", index, name="home"),
+
+    # User authentication
     path("register/", register, name="register"),
     path("login/", sign_in, name="login"),
     path("logout/", sign_out, name="logout"),
 
+    # Backend endpoint for searching packages
     path("accounts/search/", is_staff(account_search), name="account_search"),
 
+    # Generic package information
     path("packages/", is_staff(all_packages), name="packages"),
     path("packages/<int:pk>", is_staff(package_detail), name="package_detail"),
-    path("packages/new", is_staff(create_package), name="create_package"),
-    path("packages/checkin", is_staff(check_in_packages), name="check_in_packages"),
-    path("packages/checkout", is_staff(check_out_packages), name="check_out_packages"),
-    path("packages/checkout/search", is_staff(search_check_out_packages), name="search_check_out_packages"),
+
+    # Search page
     path("packages/search", is_staff(search_packages), name="search_packages"),
 
+    # Check in page
+    path("packages/checkin", is_staff(check_in_packages), name="check_in_packages"),
+    path("packages/new", is_staff(create_package), name="create_package"),
+
+    # Check out page
+    path("packages/checkout", is_staff(check_out_packages), name="check_out_packages"),
+    path("packages/checkout/search", is_staff(search_check_out_packages), name="search_check_out_packages"),
+
+    # Label printing
     path("packages/label", is_staff(generate_label), name="generate_label"),
 ]
