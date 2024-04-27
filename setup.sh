ed6c43f30c3c9f70503e86d9a7fb2a90b91600ae
Simon Quigley <squigley@altispeed.com>
2024-04-27 19:43:31 +0000

    Tweaks to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,52 +1,69 @@
 #!/bin/bash
 
-function migrate () {
-    python3 manage.py migrate
+VENV_DIR="env"
+PYTHON_PATH="$VENV_DIR/bin/python3"
+
+function setup_virtualenv() {
+    if [ ! -d "$VENV_DIR" ]; then
+        echo "Setting up the virtual environment..."
+        virtualenv $VENV_DIR
+    fi
 }
 
-function init () {
-    python3 manage.py loaddata initial_data.json
-}
-
-function load_testdata () {
-    python3 manage.py loaddata package_seed_data.json
-}
-
-function check () {
-    python3 manage.py check --deploy
-}
-
-function collectstatic () {
-    python3 manage.py collectstatic --no-input
-}
-
-if [ "$2" != "" ]
-then
-  source $2/bin/activate
-fi
-
-if [ "$1" = "prod" ]
-then
-  migrate
-  init
-  collectstatic
-elif [ "$1" = "dev" ]
-then
-  migrate
-  init
-  load_testdata
-  collectstatic
-elif [ "$1" = "update" ]
-then
-  migrate
-  collectstatic
-elif [ "$1" = "check" ]
-then
-  check
+# Check if virtualenv is installed and setup or setup if not already
+if command -v virtualenv &>/dev/null; then
+    setup_virtualenv
 else
-  echo "Available Commands:"
-  echo "    check    Run a system check to indentity issues for a production instance"
-  echo "    dev      Initialize a new development setup"
-  echo "    prod     Initialize a new production setup"
-  echo "    update   Perform a database migration in preparation for updates"
+    echo "virtualenv is not installed. Please install it and try again."
+    exit 1
 fi
+
+# Define functions for various setup tasks
+function migrate() {
+    $PYTHON_PATH manage.py migrate
+}
+
+function init() {
+    $PYTHON_PATH manage.py loaddata initial_data.json
+}
+
+function load_testdata() {
+    $PYTHON_PATH manage.py loaddata package_seed_data.json
+}
+
+function check() {
+    $PYTHON_PATH manage.py check --deploy
+}
+
+function collectstatic() {
+    $PYTHON_PATH manage.py collectstatic --no-input
+}
+
+# Use case statement to process commands
+case "$1" in
+    prod)
+        migrate
+        init
+        collectstatic
+        ;;
+    dev)
+        migrate
+        init
+        load_testdata
+        collectstatic
+        ;;
+    update)
+        migrate
+        collectstatic
+        ;;
+    check)
+        check
+        ;;
+    *)
+        echo "Available Commands:"
+        echo "    check    Run a system check to identify issues for a production instance"
+        echo "    dev      Initialize a new development setup"
+        echo "    prod     Initialize a new production setup"
+        echo "    update   Perform a database migration in preparation for updates"
+        ;;
+esac
