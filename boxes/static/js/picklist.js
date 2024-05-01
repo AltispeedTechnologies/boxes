5414b17f517cb9b937a3db239a8919b32ec2f5b3
Simon Quigley <squigley@altispeed.com>
2024-05-01 03:29:00 +0000

    Fix duplicate naming

--- boxes/static/js/picklist.js
+++ boxes/static/js/picklist.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     let csrf_token = window.get_cookie("csrftoken");
 
     if (typeof picklist_data !== "undefined") {
-        $("#picklist-select").select2({
+        $("#picklist-select-view").select2({
             data: picklist_data
         });
 
-        $("#picklist-select").data("select2").$container.addClass("my-2 float-end");
+        $("#picklist-select-view").data("select2").$container.addClass("my-2 float-end");
     }
 
     $(document).on("selectedPackagesUpdated", function(event) {
@@ -17,46 +17,46 @@ $(document).ready(function() {
     $("#picklistview").click(function(event) {
         event.preventDefault();
         let base_url = "/packages/picklists/";
-        let picklist_id = $("#picklist-select").find(":selected").val();
+        let picklist_id = $("#picklist-select-view").find(":selected").val();
         window.location.href = base_url + picklist_id;
     });
 
     $("#picklistbtn").click(function(event) {
         event.preventDefault();
         let packages_array = Array.from(window.selected_packages);
-        let picklist_id = $("#picklist-select").find(":selected").val();
-        let packages_payload = {
-            "ids": packages_array,
-            "picklist_id": picklist_id
-        };
-        
-        $.ajax({
-            type: "POST",
-            url: "/packages/picklists/add",
-            headers: {"X-CSRFToken": csrf_token},
-            data: JSON.stringify(packages_payload),
-            contentType: "application/json", // Specify the content type
-            success: function(response) {
-                if (response.success) {
-                    window.location.href = response.redirect_url;
-                } else {
-                    window.display_error_message(response.errors);
+            let picklist_id = $("#picklist-select-view").find(":selected").val();
+            let packages_payload = {
+                "ids": packages_array,
+                "picklist_id": picklist_id
+            };
+            
+            $.ajax({
+                type: "POST",
+                url: "/packages/picklists/add",
+                headers: {"X-CSRFToken": csrf_token},
+                data: JSON.stringify(packages_payload),
+                contentType: "application/json", // Specify the content type
+                success: function(response) {
+                    if (response.success) {
+                        window.location.href = response.redirect_url;
+                    } else {
+                        window.display_error_message(response.errors);
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error("An error occurred:", error);
                 }
-            },
-            error: function(xhr, status, error) {
-                console.error("An error occurred:", error);
-            }
+            });
         });
-    });
 
-    $("[data-bs-target=\"#moveModal\"]").on("click", function() {
-        row_id = $(this).data("row-id"); // Capture the row ID from button
-    });
+        $("[data-bs-target=\"#moveModal\"]").on("click", function() {
+            row_id = $(this).data("row-id"); // Capture the row ID from button
+        });
 
-    // Handle "Save Changes" click in the modal
-    $("#moveModal .btn-primary").on("click", function() {
-        // Get the selected item's ID from the dropdown
-        var selected_item_value = $("#picklist-select").val();
+        // Handle "Save Changes" click in the modal
+        $("#moveModal .btn-primary").on("click", function() {
+            // Get the selected item's ID from the dropdown
+            var selected_item_value = $("#picklist-select-view").val();
 
         // Prepare data for POST request
         var post_data = {
