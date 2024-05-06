210526d4e898bcd034dad76c5adfdf369603e2f2
Simon Quigley <squigley@altispeed.com>
2024-05-06 20:02:30 +0000

    Put the check in/out states back into the view module

--- boxes/tasks.py
+++ boxes/tasks.py
@@ -55,30 +55,3 @@ def send_email(account_id, package_id):
         results.append(result.json())
 
     return results
-
-@shared_task
-def update_packages_background(ids, user_id, state, debit_credit_switch=False):
-    Package.objects.filter(id__in=ids).update(current_state=state)
-    account_ledger, package_ledger, affected_accounts = [], [], set()
-    for pkg in Package.objects.filter(id__in=ids).values("id", "account_id", "price"):
-        debit, credit = (0, pkg["price"]) if debit_credit_switch else (pkg["price"], 0)
-        acct_entry = AccountLedger(user_id=user_id, package_id=pkg["id"],
-                                   account_id=pkg["account_id"], debit=debit, credit=credit, description="")
-        pkg_entry = PackageLedger(user_id=user_id, package_id=pkg["id"], state=state)
-        account_ledger.append(acct_entry)
-        package_ledger.append(pkg_entry)
-        affected_accounts.add(pkg["account_id"])
-
-    AccountLedger.objects.bulk_create(account_ledger)
-    PackageLedger.objects.bulk_create(package_ledger)
-
-    # Update balances for affected accounts
-    accounts = Account.objects.filter(id__in=affected_accounts).annotate(
-        total_credit=Sum("accountledger__credit", default=0),
-        total_debit=Sum("accountledger__debit", default=0)
-    )
-    for account in accounts:
-        new_balance = account.total_credit - account.total_debit
-        if new_balance != account.balance:
-            account.balance = new_balance
-            account.save(update_fields=["balance"])
