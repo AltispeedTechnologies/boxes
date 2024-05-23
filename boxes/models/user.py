4e24d3013eea01706ff7e09f83c6d77255670f6f
Simon Quigley <squigley@altispeed.com>
2024-05-23 22:22:00 +0000

    New fields required for data import

--- boxes/models/user.py
+++ boxes/models/user.py
@@ -4,6 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     company = models.CharField(max_length=128, null=True)
     phone_number = models.CharField(max_length=20, null=True)
+    mobile_number = models.CharField(max_length=20, null=True)
     prefix = models.CharField(max_length=16, null=True)
     middle_name = models.CharField(max_length=64, null=True)
     suffix = models.CharField(max_length=16, null=True)
@@ -16,3 +17,7 @@ class CustomUser(AbstractUser):
 
     def is_staff(self):
         return self.groups.filter(name="Admin").exists() or self.groups.filter(name="Staff").exists()
+
+class CustomUserEmail(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.RESTRICT)
+    email = models.EmailField(blank=False, max_length=254)
