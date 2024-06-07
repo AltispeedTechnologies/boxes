4e4aaca8d014623d4a0d57e25ce4f24feffd28ac
Simon Quigley <squigley@altispeed.com>
2024-06-07 21:13:16 +0000

    10 emails per page, not 15

--- boxes/views/mgmt/email.py
+++ boxes/views/mgmt/email.py
@@ -48,7 +48,7 @@ def email_logs(request):
         email.tracking_codes = tracking_codes
 
     page_number = request.GET.get("page")
-    paginator = Paginator(emails, 15)
+    paginator = Paginator(emails, 10)
     page_obj = paginator.get_page(page_number)
 
     return render(request, "mgmt/email_logs.html", {"page_obj": page_obj,
