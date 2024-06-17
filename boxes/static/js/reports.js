465c3666bf4c3f968cf1e56fdc6591fba4044b39
Simon Quigley <squigley@altispeed.com>
2024-06-17 18:00:09 +0000

    Revert "Rework: prep for the edit modal"
    This reverts commit 72be5c0c85a8dcc98bcde9f8d5b9b8be16b5e37a.

--- boxes/static/js/reports.js
+++ boxes/static/js/reports.js
@@ -1,25 +1,22 @@
-function prepare_json_payload(modal_name) {
-    // Grab the selected modal
-    var $modal = $(modal_name);
-
+function prepare_json_payload() {
     // Specific format enforced on the backend
     let config = {
         fields: [],
     };
 
     // Report name from user input
-    let name = $modal.find("#reportname").val();
+    let name = $("#reportname").val();
 
     // Find all selected fields in order
-    $modal.find("tbody#field_order").find("tr:not(.visually-hidden)").each(function() {
+    $("#newReportModal").find("tbody#field_order").find("tr:not(.visually-hidden)").each(function() {
         config["fields"].push(this.id);
     });
 
     // Find the selected "sort by field" entry
-    config["sort_by"] = $modal.find("#sort_by_field").val();
+    config["sort_by"] = $("#newReportModal").find("#sort_by_field").val();
 
     // Find the "filter by date" selection and store the values appropriately
-    let filter_by_date = $modal.find("input[name=\"filter_by_date\"]:checked").attr("id");
+    let filter_by_date = $("#newReportModal").find("input[name=\"filter_by_date\"]:checked").attr("id");
     config["filter"] = {};
 
     switch(filter_by_date) {
@@ -29,14 +26,14 @@ function prepare_json_payload(modal_name) {
         case "date_range":
             config["filter"]["type"] = filter_by_date;
 
-            let date_range_from = $modal.find("#date_range_from").val();
-            let date_range_to = $modal.find("#date_range_to").val();
+            let date_range_from = $("#date_range_from").val();
+            let date_range_to = $("#date_range_to").val();
             const date_range_from_obj = new Date(date_range_from);
             const date_range_to_obj = new Date(date_range_to);
 
             if (date_range_from_obj > date_range_to_obj) {
-                $modal.find("#select_date_range").find(".form-control").addClass("is-invalid");
-                $modal.find("#select_date_range").find(".invalid-feedback").text("First date must be before (or equal to) the second").show();
+                $("#select_date_range").find(".form-control").addClass("is-invalid");
+                $("#select_date_range").find(".invalid-feedback").text("First date must be before (or equal to) the second").show();
                 return;
             }
 
@@ -47,11 +44,11 @@ function prepare_json_payload(modal_name) {
         case "relative_date_range":
             config["filter"]["type"] = filter_by_date;
 
-            let relative_date_range_from = Number($modal.find("#relative_date_range_from").val());
-            let relative_date_range_to = Number($modal.find("#relative_date_range_to").val());
+            let relative_date_range_from = Number($("#relative_date_range_from").val());
+            let relative_date_range_to = Number($("#relative_date_range_to").val());
             if (relative_date_range_to <= relative_date_range_from) {
-                $modal.find("#select_relative_date_range").find(".form-control").addClass("is-invalid");
-                $modal.find("#select_relative_date_range").find(".invalid-feedback").text("Start value must be greater than end value").show();
+                $("#select_relative_date_range").find(".form-control").addClass("is-invalid");
+                $("#select_relative_date_range").find(".invalid-feedback").text("Start value must be greater than end value").show();
                 return;
             }
 
@@ -61,7 +58,7 @@ function prepare_json_payload(modal_name) {
             break;
         case "time_period":
             config["filter"]["type"] = filter_by_date;
-            let time_period_selected = $modal.find("input[name=\"time_period\"]:checked").attr("id");
+            let time_period_selected = $("#newReportModal").find("input[name=\"time_period\"]:checked").attr("id");
             config["filter"]["frequency"] = time_period_selected.replace("time_period_", "");
             break;
     }
@@ -80,59 +77,54 @@ function toggle_disabled_chart_buttons(disabled) {
     $("#monthbtn").attr("disabled", disabled);
 }
 
-function toggle_create_button(modal_name) {
-    // Grab the selected modal
-    var $modal = $(modal_name);
-
+function toggle_create_button() {
     // Ensure a field is selected
-    let create_disabled = $modal.find("#display_fields input[type=\"checkbox\"]:checked").length === 0;
+    let create_disabled = $("#display_fields input[type=\"checkbox\"]:checked").length === 0;
 
     // Also ensure a name has been entered
-    create_disabled = create_disabled || ($modal.find("#reportname").val() == "");
+    create_disabled = create_disabled || ($("#reportname").val() == "");
 
-    $modal.find(".btn-primary").attr("disabled", create_disabled);
+    $("#newReportModal .btn-primary").attr("disabled", create_disabled);
 }
 
-function toggle_row_arrows(modal_name) {
-    var $modal = $(modal_name);
-
+function toggle_row_arrows() {
     // Enable all buttons
-    $modal.find(".move-up").prop("disabled", false);
-    $modal.find(".move-down").prop("disabled", false);
+    $(".move-up").prop("disabled", false);
+    $(".move-down").prop("disabled", false);
 
     // Disable the first move up button
-    $modal.find(".move-up").first().prop("disabled", true);
+    $(".move-up").first().prop("disabled", true);
 
     // Disable the last move down button
-    $modal.find(".move-down").last().prop("disabled", true);
+    $(".move-down").last().prop("disabled", true);
 }
 
-function clear_modal(modal_name) {
-    var $modal = $(modal_name);
+function clear_create_modal() {
+    var $new_modal = $("#newReportModal");
 
     // Clear the report name
-    $modal.find("#reportname").val("");
+    $new_modal.find("#reportname").val("");
 
     // Deselect all fields
-    $modal.find("#display_fields input[type=\"checkbox\"]:checked").prop("checked", false);
+    $new_modal.find("#display_fields input[type=\"checkbox\"]:checked").prop("checked", false);
 
     // Remove all dropdown items
-    $modal.find("#sort_by_field").find("option").remove();
-    $modal.find("#sort_by_field").trigger("change");
+    $new_modal.find("#sort_by_field").find("option").remove();
+    $new_modal.find("#sort_by_field").trigger("change");
 
     // Remove all ordering options
-    $modal.find("tbody#field_order").find("tr:not(.visually-hidden)").remove();
+    $new_modal.find("tbody#field_order").find("tr:not(.visually-hidden)").remove();
 
     // Ensure default of All for dates is set
-    $modal.find("input[name=\"filter_by_date\"]:checked").prop("checked", false);    
-    $modal.find("input[name=\"filter_by_date\"][id=\"all_entries\"]").prop("checked", true);
+    $new_modal.find("input[name=\"filter_by_date\"]:checked").prop("checked", false);    
+    $new_modal.find("input[name=\"filter_by_date\"][id=\"all_entries\"]").prop("checked", true);
 
     // Hide all subselection divs
-    $modal.find("div.filter_subselection").hide();
+    $new_modal.find("div.filter_subselection").hide();
 
     // Clear all subselection divs
-    $modal.find("div.filter_subselection").find("input").val("");
-    $modal.find("div.filter_subselection").find("input:checked").prop("checked", false);
+    $new_modal.find("div.filter_subselection").find("input").val("");
+    $new_modal.find("div.filter_subselection").find("input:checked").prop("checked", false);
 }
 
 function update_chart(payload) {
@@ -161,7 +153,7 @@ function update_chart(payload) {
     });
 }
 
-function init_chart() {
+function init_reports_page() {
     // Initialize the chart
     window.mainchart = new Chart("mainchart", {
         type: "line",
@@ -208,21 +200,16 @@ function init_chart() {
         $("#weekbtn").addClass("btn-light").removeClass("btn-primary");
         $("#monthbtn").addClass("btn-primary").removeClass("btn-light");
     });
-}
-
-function init_modal(modal_name) {
-    // Act on a specific modal, so duplicate IDs can be used
-    var $modal = $(modal_name);
 
     // Basic setup for Sort By Field
-    $modal.find("#sort_by_field").select2({dropdownParent: modal_name, width: "50%"});
+    $("#sort_by_field").select2({dropdownParent: "#newReportModal", width: "50%"});
     window.select2properheight("#sort_by_field");
 
     // Handle checkbox changes
-    $modal.find("#display_fields input[type=\"checkbox\"]").off("change").on("change", function() {
-        var select2_dropdown = $modal.find("#sort_by_field");
+    $("#display_fields input[type=\"checkbox\"]").off("change").on("change", function() {
+        var select2_dropdown = $("#sort_by_field");
         var option_id = $(this).attr("id");
-        var option_text = $modal.find("label[for=\"" + option_id + "\"").text();
+        var option_text = $("label[for=\"" + option_id + "\"").text();
 
         if ($(this).is(":checked")) {
             // Add the selected item as a dropdown entry
@@ -230,14 +217,14 @@ function init_modal(modal_name) {
             select2_dropdown.append(new_option).trigger("change");
 
             // Add the new row for ordering
-            let new_row = $modal.find("tbody#field_order").find(".visually-hidden")
+            let new_row = $("tbody#field_order").find(".visually-hidden")
                 .clone()
                 .removeClass("visually-hidden")
                 .attr("id", option_id);
             new_row.find("td:nth-child(1)").text(option_text);
             new_row.find("button:nth-child(1)").addClass("move-up");
             new_row.find("button:nth-child(2)").addClass("move-down");
-            $modal.find("tbody#field_order").find(".visually-hidden").before(new_row);
+            $("tbody#field_order").find(".visually-hidden").before(new_row);
         } else {
             // Remove the option if the checkbox is unchecked
             select2_dropdown.find("option[value=\"" + option_id + "\"]").remove();
@@ -248,8 +235,8 @@ function init_modal(modal_name) {
         }
 
         // Only enable the create button if one or more fields are selected
-        toggle_create_button(modal_name);
-        toggle_row_arrows(modal_name);
+        toggle_create_button();
+        toggle_row_arrows();
     });
 
     // Handle moving a specific row up or down
@@ -260,7 +247,7 @@ function init_modal(modal_name) {
             row.prev().before(row);
         }
 
-        toggle_row_arrows(modal_name);
+        toggle_row_arrows();
     });
     $(document).on("click", ".move-down", function() {
         var row = $(this).parents("tr");
@@ -270,38 +257,14 @@ function init_modal(modal_name) {
             row.next().after(row);
         }
 
-        toggle_row_arrows(modal_name);
+        toggle_row_arrows();
     });
 
-    // Ensure there is a report name and that it is unique
-    $modal.find("#reportname").off("input").on("input", window.debounce(function() {
-        toggle_create_button();
-
-        let name = $(this).val();
-        if (name !== "") {
-            let payload = JSON.stringify({"name": name});
-
-            window.ajax_request({
-                type: "POST",
-                url: "/reports/name",
-                payload: payload,
-                content_type: "application/json",
-                on_success: function(response) {
-                    if (!response.unique) {
-                        // Show an error on the screen
-                        $modal.find("#reportname").addClass("is-invalid");
-                        $modal.find("#reportname").next(".invalid-feedback").text("Duplicate report name").show();
-
-                        // Manually disable the create button
-                        $modal.find(".btn-primary").attr("disabled", true);
-                    }
-                }
-            });
-        }
-    }, 500));
+    // Hook up changes to the report name to enablement of the createbox
+    $("#reportname").off("input").on("input", function() {toggle_create_button();});
 
     // Initialize the date pickers
-    $modal.find("#select_date_range").find(".form-control").datepicker({
+    $("#select_date_range").find(".form-control").datepicker({
         // Ensure the date picker pops up in the modal
         beforeShow: function(input, inst) {
             $(inst.dpDiv).appendTo("#select_date_range");
@@ -313,56 +276,51 @@ function init_modal(modal_name) {
     });
 
     // Set up the dynamic filter selection for the add modal
-    $modal.find("input[name=\"filter_by_date\"]").off("change").on("change", function() {
+    $("input[name=\"filter_by_date\"]").off("change").on("change", function() {
         // One of: all_entries, date_range, relative_date_range, time_period
         let new_id = $(this).attr("id");
 
         // Hide all of the potential subselection divs
-        $modal.find("div.filter_subselection").hide();
+        $("div.filter_subselection").hide();
 
         // all_entries does not have additional config options, otherwise show appropriately
         if (new_id !== "all_entries") {
-            $modal.find("#select_" + new_id).show();
+            $("#select_" + new_id).show();
         }
     });
 
-    // Specific submission actions
-    if (modal_name === "#newReportModal") {
-        $("#newReportModal .btn-primary").off("click").on("click", function() {
-            const [payload, name] = prepare_json_payload(modal_name);
+    $("#newReportModal .btn-primary").off("click").on("click", function() {
+        const [payload, name] = prepare_json_payload();
 
-            window.ajax_request({
-                type: "POST",
-                url: "/reports/new",
-                payload: JSON.stringify(payload),
-                content_type: "application/json",
-                on_success: function(response) {
-                    // If the table is not present, a reload is easier
-                    if ($("tbody#reports").length === 0) {
-                        window.location.reload();
-                    }
-     
-                    // Add it visually
-                    let new_row = $("tbody#reports").find(".visually-hidden")
-                        .clone()
-                        .removeClass("visually-hidden")
-                        .attr("data-id", response.id);
-                    new_row.find("td:nth-child(1)").text(name);
-                    $("tbody#reports").find(".visually-hidden").before(new_row);
-
-                    // Clear the modal
-                    clear_modal(modal_name);
-                },
-                on_response: function() {
-                    // Hide the modal
-                    $modal.modal("hide");
+        window.ajax_request({
+            type: "POST",
+            url: "/reports/new",
+            payload: JSON.stringify(payload),
+            content_type: "application/json",
+            on_success: function(response) {
+                // If the table is not present, a reload is easier
+                if ($("tbody#reports").length === 0) {
+                    window.location.reload();
                 }
-            });
-        });
-    }
-}
+ 
+                // Add it visually
+                let new_row = $("tbody#reports").find(".visually-hidden")
+                    .clone()
+                    .removeClass("visually-hidden")
+                    .attr("data-id", response.id);
+                new_row.find("td:nth-child(1)").text(name);
+                $("tbody#reports").find(".visually-hidden").before(new_row);
 
-function init_remove_modal() {
+                // Clear the create modal
+                clear_create_modal();
+            },
+            on_response: function() {
+                // Hide the modal
+                $("#newReportModal").modal("hide");
+            }
+        });
+    });
+
     // Store the row ID for deletion
     let current_id = 0;
 
@@ -385,7 +343,5 @@ function init_remove_modal() {
 }
 
 if ($("div#reportspage").length !== 0) {
-    init_chart();
-    init_modal("#newReportModal");
-    init_remove_modal();
+    init_reports_page();
 }
