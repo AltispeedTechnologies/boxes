6fcd40152f3e3462e4085bfffe86f9c8f7ec0653
Simon Quigley <squigley@altispeed.com>
2024-06-28 16:50:34 +0000

    Fix pycodestyle lint

--- boxes/management/commands/processjs.py
+++ boxes/management/commands/processjs.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
+
 class Command(BaseCommand):
     help = "Runs collectstatic and cleans up old files"
 
