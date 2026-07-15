315255fd6e8580be567921e22ed84d17da3ad31c
Simon Quigley <squigley@altispeed.com>
2026-07-15 14:22:31 +0000

    Confirm over/under payments with Bootstrap modals
    Replace console.log placeholders with confirm dialogs before checkout.

--- boxes/static/js/customer/make_payment.js
+++ boxes/static/js/customer/make_payment.js
@@ -4,22 +4,59 @@
  * @see docs/api/javascript.md
  */
 
+function submit_checkout(payment_amount, payment_method_id) {
+    $("#checkoutloading").removeClass("d-none");
+    $("button#checkoutbtn").attr("disabled", true);
+
+    var payload = { amount: payment_amount, method: payment_method_id };
+
+    window.ajax_request({
+        type: "POST",
+        url: "/invoice/new",
+        payload: JSON.stringify(payload),
+        content_type: "application/json",
+        on_success: function(response) {
+            window.location.href = response.url;
+        },
+        on_response: function(response) {
+            if (!response.success) {
+                $("#checkoutloading").addClass("d-none");
+                $("button#checkoutbtn").attr("disabled", false);
+            }
+        }
+    });
+}
+
+function get_selected_payment_method_id() {
+    if ($("input[name=\"paymentmethod\"]:checked").attr("id") === "onetimemethod") {
+        return "ONETIME";
+    }
+    return $("button#currentpaymentmethod").attr("data-id");
+}
+
+function get_selected_payment_amount(current_balance) {
+    var payment_amount_selection = $("input[name=\"paymentamount\"]:checked").attr("id");
+    if (payment_amount_selection === "otheramount") {
+        return parseFloat($("input#otheramountinput").val());
+    }
+    return current_balance;
+}
+
 function init_customer_payment_page() {
     $("input#otheramountinput").off("input").on("input", function() {
         window.format_price_input($(this));
     });
 
     $("input[name=\"paymentamount\"]").off("change").on("change", function() {
-        let hide_input_box = $(this).attr("id") !== "otheramount";
+        var hide_input_box = $(this).attr("id") !== "otheramount";
         $("div#enterotherinput").toggleClass("d-none", hide_input_box);
     });
 
     $("input[name=\"paymentmethod\"]").off("change").on("change", function() {
-        let hide_dropdown = $(this).attr("id") !== "selectmethod";
+        var hide_dropdown = $(this).attr("id") !== "selectmethod";
         $("#currentpaymentmethod").toggleClass("d-none", hide_dropdown);
     });
 
-    // Selecting another payment method
     $("#paymentmethodsdropdown a").off("click").on("click", function() {
         $("#currentpaymentmethod").html($(this).html());
         $("#currentpaymentmethod").attr("data-id", $(this).attr("data-id"));
@@ -29,41 +66,47 @@ function init_customer_payment_page() {
     });
 
     $("button#checkoutbtn").off("click").on("click", function() {
-        $("#checkoutloading").removeClass("d-none");
-        $(this).attr("disabled", true);
-
-        let current_balance = parseFloat($("input#currentdue").data("balance"));
-
-        let payment_method_id;
-        if ($("input[name=\"paymentmethod\"]:checked").attr("id") === "onetimemethod") {
-            payment_method_id = "ONETIME";
-        } else {
-            payment_method_id = $("button#currentpaymentmethod").attr("data-id");
+        var current_balance = parseFloat($("input#currentdue").data("balance"));
+        if (isNaN(current_balance)) {
+            current_balance = 0;
         }
 
-        let payment_amount_selection = $("input[name=\"paymentamount\"]:checked").attr("id");
-        let payment_amount;
+        var payment_method_id = get_selected_payment_method_id();
+        var payment_amount = get_selected_payment_amount(current_balance);
 
-        if (payment_amount_selection === "otheramount") {
-            payment_amount = parseFloat($("input#otheramountinput").val());
+        if (isNaN(payment_amount) || payment_amount < 0.5) {
+            window.display_error_message(["Enter a valid amount of at least $0.50."]);
+            return;
+        }
+
+        var payment_amount_selection = $("input[name=\"paymentamount\"]:checked").attr("id");
+        if (payment_amount_selection === "otheramount" && current_balance > 0) {
             if (payment_amount > current_balance) {
-                console.log("more than current balance");
+                $("#over-specified").text(payment_amount.toFixed(2));
+                $("#over-due").text(current_balance.toFixed(2));
+                $("#over-credit").text((payment_amount - current_balance).toFixed(2));
+                var overModal = bootstrap.Modal.getOrCreateInstance(document.getElementById("overPaymentAmountModal"));
+                $("#overPaymentConfirm").off("click").on("click", function() {
+                    overModal.hide();
+                    submit_checkout(payment_amount, payment_method_id);
+                });
+                overModal.show();
+                return;
             }
-        } else {
-            payment_amount = current_balance;
+            if (payment_amount < current_balance) {
+                $("#under-specified").text(payment_amount.toFixed(2));
+                $("#under-due").text(current_balance.toFixed(2));
+                var underModal = bootstrap.Modal.getOrCreateInstance(document.getElementById("underPaymentAmountModal"));
+                $("#underPaymentConfirm").off("click").on("click", function() {
+                    underModal.hide();
+                    submit_checkout(payment_amount, payment_method_id);
+                });
+                underModal.show();
+                return;
+            }
         }
 
-        var payload = { amount: payment_amount, method: payment_method_id };
-
-        window.ajax_request({
-            type: "POST",
-            url: "/invoice/new",
-            payload: JSON.stringify(payload),
-            content_type: "application/json",
-            on_success: function(response) {
-                window.location.href = response.url;
-            }
-        });
+        submit_checkout(payment_amount, payment_method_id);
     });
 }
 
