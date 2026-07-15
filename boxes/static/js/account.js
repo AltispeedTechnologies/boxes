ac95317b8f096018e519c9ec8dd2a40a63590609
Simon Quigley <squigley@altispeed.com>
2026-07-15 14:22:31 +0000

    Staff account edit UI for link and disassociate members

--- boxes/static/js/account.js
+++ boxes/static/js/account.js
@@ -1,16 +1,16 @@
 /**
  * @file account.js
- * @description Staff account detail page interactions (comments save, etc.).
+ * @description Staff account detail page interactions (comments save, membership, fee waiver).
  * @see docs/api/javascript.md
  */
 
 function init_account_page() {
     $("#accountnotes").off("input").on("input", window.debounce(function() {
         $("#savingnotes").removeClass("d-none");
-        let account_id = $(this).attr("data-id");
+        var account_id = $(this).attr("data-id");
 
-        new_comments = $(this).val();
-        let payload = JSON.stringify({"comments": new_comments});
+        var new_comments = $(this).val();
+        var payload = JSON.stringify({"comments": new_comments});
 
         window.ajax_request({
             type: "POST",
@@ -26,12 +26,12 @@ function init_account_page() {
 
     if ($("input#billable").length === 1) {
         $("input#billable").off("change").on("change", function() {
-            let $input = $(this);
+            var $input = $(this);
             $input.prop("disabled", true);
             $("#savingiconbillable").show();
 
-            let acct_payload = {billable: $(this).prop("checked")};
-            let account_id = $(this).attr("data-id");
+            var acct_payload = {billable: $(this).prop("checked")};
+            var account_id = $(this).attr("data-id");
 
             window.ajax_request({
                 type: "POST",
@@ -73,6 +73,45 @@ function init_account_page() {
                         $("#successiconwaiver").fadeOut();
                         window.location.reload();
                     }, 800);
+                }
+            });
+        });
+    }
+
+    var $memberships = $("#account-memberships");
+    if ($memberships.length === 1) {
+        var membership_account_id = $memberships.data("account-id");
+
+        $("#link-member-btn").off("click").on("click", function() {
+            var user_id = $("#link-member-user-id").val();
+            var role = $("#link-member-role").val() || "member";
+            if (!user_id) {
+                window.display_error_message(["Enter a user id."]);
+                return;
+            }
+            window.ajax_request({
+                type: "POST",
+                url: "/accounts/" + membership_account_id + "/members/link",
+                payload: JSON.stringify({user_id: user_id, role: role}),
+                content_type: "application/json",
+                on_success: function() {
+                    window.location.reload();
+                }
+            });
+        });
+
+        $memberships.off("click", ".disassociate-member").on("click", ".disassociate-member", function() {
+            var user_id = $(this).data("user-id");
+            if (!window.confirm("Disassociate this user from the account?")) {
+                return;
+            }
+            window.ajax_request({
+                type: "POST",
+                url: "/accounts/" + membership_account_id + "/members/disassociate",
+                payload: JSON.stringify({user_id: user_id}),
+                content_type: "application/json",
+                on_success: function() {
+                    window.location.reload();
                 }
             });
         });
